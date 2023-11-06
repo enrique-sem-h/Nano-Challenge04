6e68ca66d65de46b34479d59ee22d1424fa510fa
LetiMalaga <leticiamalagutti@yahoo.com.br>
2023-11-06 15:21:51 -0300

    teste commit

---
+++ Nano-Challenge04/Nano-Challenge04/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nano-Challenge04
+//
+//  Created by Letícia Malagutti on 06/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nano-Challenge04/Nano-Challenge04/Nano_Challenge04App.swift
@@ -0,0 +1,17 @@
+//
+//  Nano_Challenge04App.swift
+//  Nano-Challenge04
+//
+//  Created by Letícia Malagutti on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Nano_Challenge04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
