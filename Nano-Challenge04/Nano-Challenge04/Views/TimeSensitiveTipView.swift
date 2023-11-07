dafe7b958c8acf66f812870492fe0eff48958c0b
LetiMalaga <leticiamalagutti@yahoo.com.br>
2023-11-07 14:23:25 -0300

    Arquivos das tips

---
+++ Nano-Challenge04/Nano-Challenge04/Views/TimeSensitiveTipView.swift
@@ -0,0 +1,18 @@
+//
+//  TimeSensitiveTipView.swift
+//  Nano-Challenge04
+//
+//  Created by Letícia Malagutti on 07/11/23.
+//
+
+import SwiftUI
+
+struct TimeSensitiveTipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimeSensitiveTipView()
+}
