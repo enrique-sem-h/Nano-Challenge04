e7e141ec5b2f291f65ae2a455c93a1acc76879c0
Luca Lacerda <lucagabriel520@gmail.com>
2023-11-07 15:41:48 -0300

    Tip por tempo

--- Nano-Challenge04/Nano-Challenge04/Views/TimeSensitiveTipView.swift
+++ Nano-Challenge04/Nano-Challenge04/Views/TimeSensitiveTipView.swift
@@ -6,10 +6,46 @@
 //
 
 import SwiftUI
+import TipKit
+
+struct TimeSensitiveTip:Tip {
+    @Parameter static var hasTimeElapsed: Bool = false
+    
+    var title: Text = Text("Tempo")
+    var message: Text? = Text("teste")
+    
+    public mutating func delayText() async {
+        // Delay of 7.5 seconds (1 second = 1_000_000_000 nanoseconds)
+        try? await Task.sleep(nanoseconds: 5_000_000_000)
+        TimeSensitiveTip.hasTimeElapsed = true
+    }
+    
+    var rules: [Rule]{
+        [
+            #Rule(TimeSensitiveTip.$hasTimeElapsed) {
+                $0 == true
+            }
+        ]
+    }
+}
 
 struct TimeSensitiveTipView: View {
+    
+    @State var tip = TimeSensitiveTip()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .popoverTip(tip)
+        }.onAppear{
+            Task{
+                try? Tips.configure([
+                    .displayFrequency(.immediate),
+                ])
+//                Tips.showAllTipsForTesting()
+                await tip.delayText()
+            }
+        }
     }
 }
 
