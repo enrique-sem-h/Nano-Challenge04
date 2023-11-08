344c7dffddb8b3dd531f0df36102bdb57a034903
Luca Lacerda <lucagabriel520@gmail.com>
2023-11-08 14:41:49 -0300

    commit final

--- Nano-Challenge04/Nano-Challenge04/Nano_Challenge04App.swift
+++ Nano-Challenge04/Nano-Challenge04/Nano_Challenge04App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Nano_Challenge04App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TimeSensitiveTipView()
         }
     }
 }
